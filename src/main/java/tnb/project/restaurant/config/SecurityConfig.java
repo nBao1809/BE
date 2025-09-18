879835fdbd537945ba092eddddcb9ff5a56bb4e9
nBao <tonhatbao@gmail.com>
2025-09-19 03:06:51 +0700

    fix cors

--- src/main/java/tnb/project/restaurant/config/SecurityConfig.java
+++ src/main/java/tnb/project/restaurant/config/SecurityConfig.java
@@ -2,15 +2,22 @@ package tnb.project.restaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import tnb.project.restaurant.Filter.JwtAuthenticationFilter;
 import tnb.project.restaurant.Filter.ApiLoggingFilter;
 
+import java.util.List;
+
 import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -28,10 +35,20 @@ public class SecurityConfig {
     public ApiLoggingFilter apiLoggingFilter() {
         return new ApiLoggingFilter();
     }
-
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://your-frontend-domain.com")); // FE domain
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter, ApiLoggingFilter apiLoggingFilter) throws Exception {
-        http
+        http.cors(withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 // Auth endpoints
